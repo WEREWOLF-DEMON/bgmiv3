d03ea7f9a7d5292cf15171df632e7b763eac7d99
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2025-02-02 10:32:43 +0530

    rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7102257066:AAHJ7ssWRJtlbb6oKhSf61iHsZ9Gh6Gn-vM")
+BOT_TOKEN=("7916216583:AAGSWBKrcWDZ5ZPUp2wy3ElwWdhA1bakw8E")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Ayush143")
+OWNER_USERNAME=("@WerewolfDemon")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("859818729")
+ADMIN_IDS=("6656608288")
 
 
 
